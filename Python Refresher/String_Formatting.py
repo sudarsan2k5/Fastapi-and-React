041629930d894c623175032b48db35f1344f4b05
Sudarsan Mansingh <sudarsan2k5@gmail.com>
2026-06-01 23:17:53 +0530

    floor division

--- Python Refresher/String_Formatting.py
+++ Python Refresher/String_Formatting.py
@@ -32,3 +32,29 @@ print(f"Hi {first_name} {last_name} I am Spider Man :)")
 
 
 
+# String Assignment
+""" - Ask the user how many days until their birthday
+
+- Using the print()function. Print an approx. number of weeks until their birthday
+
+- 1 week is = to 7 days.
+
+"""
+
+days = int(input("How many days until your birthday?"))
+
+weeks = days // 7
+
+print(f"Your birthday is in {weeks} weeks")
+
+
+"""
+// is called floor division (or integer division).
+It divides two numbers and returns the whole number part only, removing any decimal.
+
+Example:
+
+print(10 / 3) => 3.3333333333333335
+print(10 // 3) => 3
+
+"""
